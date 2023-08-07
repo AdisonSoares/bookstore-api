593bb39a4e94c4e8624b8cc8dfb7646d9d83d307
adison <adison.soares7@gmail.com>
2023-08-07 18:55:22 -0300

    :sparkles: Feat: finalizing domain classes
    getting started with jpa annotations

--- bookstore/src/main/java/com/adison/bookstore/domain/Categoria.java
+++ bookstore/src/main/java/com/adison/bookstore/domain/Categoria.java
@@ -1,7 +1,10 @@
 package com.adison.bookstore.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,14 +12,22 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Categoria {
+@Entity
+public class Categoria  implements Serializable {
 
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Integer id;
 
+    @OneToMany(mappedBy = "categoria")
+    private List<Livro> livros = new ArrayList<>();
+
     private String nome;
     private String descricao;
-    private List<Livro> livros = new ArrayList<>();
 
     public Categoria(Integer id, String nome, String descricao){
         this.id = id;
--- bookstore/src/main/java/com/adison/bookstore/domain/Livro.java
+++ bookstore/src/main/java/com/adison/bookstore/domain/Livro.java
@@ -1,19 +1,46 @@
 package com.adison.bookstore.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Livro {
-    
+public class Livro implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Integer id;
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
 
     private String titulo;
     private String nome_autor;
     private String texto;
-    private Categoria categoria;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
