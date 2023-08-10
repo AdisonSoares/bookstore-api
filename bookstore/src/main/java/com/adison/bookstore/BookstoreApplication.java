88abe7fe1c0a7743002c34a89d67fc9305beccb9
adison <adison.soares7@gmail.com>
2023-08-10 12:53:32 -0300

    :art: Refactor: code cleanup
    removal of insert codes

--- bookstore/src/main/java/com/adison/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/adison/bookstore/BookstoreApplication.java
@@ -1,37 +1,11 @@
 package com.adison.bookstore;
 
-import com.adison.bookstore.domain.Categoria;
-import com.adison.bookstore.domain.Livro;
-import com.adison.bookstore.repositories.CategoriaRepository;
-import com.adison.bookstore.repositories.LivroRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-
-	@Autowired
-	private LivroRepository livroRepository;
-
+public class BookstoreApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria categoria_1 = new Categoria(null, "Informática", "Livros de TI");
-		Livro livro_1 = new Livro(null, categoria_1, "Clean code", "Robert C.Martin", "Loren ipsum");
-
-		categoria_1.getLivros().addAll(Arrays.asList(livro_1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(categoria_1));
-		this.livroRepository.saveAll(Arrays.asList(livro_1));
 	}
 }
